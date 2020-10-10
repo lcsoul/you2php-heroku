6344565a372724e865f90ce117cbcb186101eeb3
lcsoul <>
2020-10-10 11:06:38 +0800

    修改

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyCG8yeUtZzjhV3u-vuuOqndSOWYZUG9Vl4');//这里改成您的api key
 define('GJ_CODE', 'TW');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
+define('SITE_NAME', 'hash');//网站名字
+define('TITLENAME', 'hash');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4jdrOIUHuihrth');//一个随机字符串，加解密KEY
 define('EMAIL', '123@123.com');//邮箱
 ?>
