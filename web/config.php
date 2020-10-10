94e3cd2c17ca1fb46c716df261c0cd77d9f977f0
lcsoul <lc_soul@qq.com>
2020-10-10 10:50:25 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyAjj6BIAz8h7fl4pix-20ni_lSgrLqfYDw');//这里改成您的api key
+define('APIKEY', 'AIzaSyCG8yeUtZzjhV3u-vuuOqndSOWYZUG9Vl4');//这里改成您的api key
 define('GJ_CODE', 'TW');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
